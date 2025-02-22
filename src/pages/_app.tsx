aef5998d1bb1e51722b053857fe0b43f45834a57
Danyal Prout <me@dany.al>
2025-02-21 18:29:40 -0600

    chore: disable server side rendering

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import "@/styles/globals.css";
-import type {AppProps} from "next/app";
+import type { AppProps } from "next/app";
+import dynamic from "next/dynamic";
+import React from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
+const App = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
-}
+};
+
+export default dynamic(() => Promise.resolve(App), {
+  ssr: false,
+});
